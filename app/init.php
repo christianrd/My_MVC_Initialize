d588e7abd1eb25779f29f834c0e395544b33a482
Christianrd <christiandevcode@gmail.com>
2016-07-29 20:53:48 -0400

    adding MVC project

---
+++ app/init.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ChristianDevCode
+ * Date: 7/29/2016
+ * Time: 5:34 PM
+ */
+
+
+//require_once ('');
+
+require_once ('core/App.php');
+
+require_once ('core/Controller.php');
+
+define('SITE_URL', 'http://'. $_SERVER['HTTP_HOST'] .'/My_MVC_Initialize'); //you can change this route url
---
+++ public/index.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ChristianDevCode
+ * Date: 7/29/2016
+ * Time: 6:20 PM
+ */
+
+require_once ('../app/init.php');
+
+$app = new App;
